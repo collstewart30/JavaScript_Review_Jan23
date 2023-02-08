5282632fe624bfbc817f3a2f70f4d37f2649693b
Colleen Stewart <collstewart30@gmail.com>
2023-02-08 15:47:27 -0500

    isAgeGreater function added to index.js

--- index.js
+++ index.js
@@ -2,10 +2,29 @@
 
 console.log("hello world");
 
-function counterPrinter(userInput){
-    for (let i=0; i < userInput; i++){
-        console.log(`My new favorite number is ${i}`)
-    }
+// function counterPrinter(userInput){
+//     for (let i=0; i < userInput; i++){
+//         console.log(`My new favorite number is ${i}`)
+//     }
+// }
+
+// counterPrinter(parseInt(prompt("Please enter a whole number between 1 and 1,000🎇")))
+
+function ageIsGreater(userInput) {
+  if (userInput > 28) {
+    return true;
+  }
+  return false;
 }
 
-counterPrinter(parseInt(prompt("Please enter a whole number between 1 and 1,000🎇")))
+let isAgeGreater = ageIsGreater(
+  parseInt(
+    prompt("Let's see if you are older than I am. \n Please enter your age.")
+  )
+);
+
+if (isAgeGreater) {
+    alert("Congrats")
+} else {
+    alert("you lose")
+}
